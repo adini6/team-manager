0d4b4ef440ba946e7f6a3f556b69a81c9898f85f
Abdikalik Dini <adini18@gmail.com>
2023-09-27 18:49:50 -0400

    edited connection.js

--- connection.js
+++ connection.js
@@ -1,8 +1,7 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3306,
   user: 'root', 
   password: 'password123', 
   database: 'employee_db',
